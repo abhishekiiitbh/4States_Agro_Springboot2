c771fffb26b8d9a6057cdd4544289a6d5e66cf98
abhishekiiitbh <b116001@iiit-bh.ac.in>
2020-11-06 13:24:15 +0530

    added biddercontroller class

--- 4StatesAgro/src/main/java/com/lti/agro/controller/BidderController.java
+++ 4StatesAgro/src/main/java/com/lti/agro/controller/BidderController.java
@@ -1,10 +1,15 @@
 package com.lti.agro.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 public class BidderController {
-
+	
+	//@Autowired
+	public static  void function() {
+		System.out.println("testing");
+	}
 }
