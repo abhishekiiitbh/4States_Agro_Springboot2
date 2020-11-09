3ee18c4ef64fb04cbb70c2d6e4055f613a8e1aea
viganeshahr <14vishalganesh98@gmail.com>
2020-11-09 20:58:52 +0530

    ForgotPassword Updated!

---
+++ 4StatesAgro/src/main/java/com/lti/agro/controller/ForgotPasswordController.java
@@ -0,0 +1,53 @@
+package com.lti.agro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.agro.dto.Status;
+import com.lti.agro.dto.Status.StatusType;
+import com.lti.agro.services.ForgotPasswordServices;
+
+@RestController
+@CrossOrigin
+public class ForgotPasswordController {
+	
+	@Autowired
+	ForgotPasswordServices forgotPasswordService;
+	
+	@PostMapping(path = "/forgotPassword")
+	public Status forgotPassword(@RequestParam("email")String email,@RequestParam("userType") String userType) {
+		boolean result=forgotPasswordService.forgotPasswordEmailCheck(email, userType);
+		Status status = new Status();
+		if(result)
+		{
+			status.setStatus(StatusType.SUCCESS);
+			status.setMessage("OTP HAS BEEN SENT SUCCESSFULLY!");
+		}
+		else
+		{
+			status.setStatus(StatusType.FAILURE);
+			status.setMessage("EMAIL IS NOT REGISTERED!");
+		}
+		return status;
+	}
+	
+	@PostMapping(path = "/resetPassword")
+	public Status resetPassword(@RequestParam("otp") String otp,@RequestParam("user") String userType,@RequestParam("password") String password) {
+		boolean result = forgotPasswordService.resetPassword(otp, password, userType);
+		Status status = new Status();
+		if(result)
+		{
+			status.setStatus(StatusType.SUCCESS);
+			status.setMessage("PASSWORD CHANGED SUCCESSFULLY!");
+		}
+		else
+		{
+			status.setStatus(StatusType.FAILURE);
+			status.setMessage("OTP ENTERED WAS WRONG!");
+		}
+		return status;
+	}
+}
---
+++ 4StatesAgro/src/main/java/com/lti/agro/services/ForgotPasswordServices.java
@@ -0,0 +1,84 @@
+package com.lti.agro.services;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lti.agro.entity.Bidder;
+import com.lti.agro.entity.Farmer;
+import com.lti.agro.repository.BidderDaoImpl;
+import com.lti.agro.repository.FarmerDaoImpl;
+
+@Service
+public class ForgotPasswordServices {
+	
+	@Autowired
+	EmailService emailService;
+	
+	@Autowired
+	FarmerDaoImpl farmerDao;
+	
+	@Autowired
+	BidderDaoImpl bidderDao;
+	
+	static Farmer farmer; 
+	
+	static Bidder bidder;
+ 	
+	static String OTP;
+	
+	public boolean forgotPasswordEmailCheck(String email,String userType)
+	{
+		if(userType.compareTo("Farmer")==0)
+		{
+			try {
+				System.out.println("Inside");
+				farmer=farmerDao.findFarmerByEmaiId(email);
+				String generator = UUID.randomUUID().toString();
+				String generated[] = generator.split("-");
+				OTP = generated[3];
+				String text="Your OTP to reset Password is: "+OTP;
+				String subject = "OTP!! FORGOT PASSWORD!!";
+				emailService.sendEmailForNewRegistration(farmer.getEmail(), text, subject);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return false;
+			}
+			return true;
+		}
+		
+		else if(userType.compareTo("Bidder")==0)
+		{
+			try {
+				bidder=bidderDao.findBidderByEmaiId(email);
+				String generator = UUID.randomUUID().toString();
+				String generated[] = generator.split("-");
+				OTP = generated[3];
+				String text="Your OTP to reset Password is: "+OTP;
+				String subject = "OTP!! FORGOT PASSWORD!!";
+				emailService.sendEmailForNewRegistration(bidder.getEmail(), text, subject);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean resetPassword(String otp,String newPassword,String userType) {
+		if(userType.compareTo("Farmer")==0&&otp.compareTo(OTP)==0)
+		{
+			farmer.setPassword(newPassword);
+			farmerDao.addOrUpdate(farmer);
+			return true;
+		}
+		else if(userType.compareTo("Bidder")==0&&otp.compareTo(OTP)==0) {
+			bidder.setPassword(newPassword);
+			bidderDao.addOrUpfateBidder(bidder);
+			return true;
+		}
+		return false;
+	}
+}	
